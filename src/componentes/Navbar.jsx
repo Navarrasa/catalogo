568a2965bdf3087fd2bafcb59f381deed0290fa3
Gustavo Bruno <gugubrunopf@gmail.com>
2025-04-24 11:34:22 -0300

    url changes

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,29 +1,29 @@
 import estilos from './navbar.module.css';
 import { Link } from 'react-router-dom';
 
-export function Navbar(){
-    return(
-        <nav className={estilos.conteiner}>
-            <ul>
-                <Link className={estilos.text_lines} to='/'>
-                    <li>
-                         <span className="material-symbols-outlined">home</span>
-                         Home
-                     </li>
-                 </Link>
-                 <Link className={estilos.text_lines} to='series'>
-                     <li>
-                         <span className="material-symbols-outlined">movie</span>
-                         Séries
-                     </li>
-                 </Link>
-                 <Link className={estilos.text_lines} to='perfil'>
-                     <li>
-                         <span className="material-symbols-outlined">person</span>
-                         Perfil
-                     </li>
-                 </Link>
-             </ul>
-         </nav>
-    );
+export function Navbar() {
+  return (
+    <nav className={estilos.conteiner}>
+      <ul>
+        <li>
+          <Link className={estilos.text_lines} to="/catalogo">
+            <span className="material-symbols-outlined">home</span>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className={estilos.text_lines} to="/catalogo/series">
+            <span className="material-symbols-outlined">movie</span>
+            Séries
+          </Link>
+        </li>
+        <li>
+          <Link className={estilos.text_lines} to="/catalogo/perfil">
+            <span className="material-symbols-outlined">person</span>
+            Perfil
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
