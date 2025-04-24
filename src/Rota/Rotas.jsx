3e5f3074c5bc1bdb7a32e3d84c74eef84e8d8982
Gustavo Bruno <gugubrunopf@gmail.com>
2025-04-24 11:32:27 -0300

    url changes

--- src/Rota/Rotas.jsx
+++ src/Rota/Rotas.jsx
@@ -1,19 +1,17 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Inicial } from '../Paginas/Inicial';
 import { Perfil } from '../Paginas/Perfil';
 import { Lista } from '../componentes/Lista';
 import { ListaSerie } from '../componentes/ListaSerie';
 
-export function Rotas(){
-    return(
-        <Routes>
-
-            <Route path = '/catalogo' element = {<Inicial/>}>
-                <Route path = '/' element = {<Lista/>}/>
-                <Route path = '/perfil' element = {<Perfil/>}/>
-                <Route path = '/series' element = {<ListaSerie/>}/> 
-            </Route>
-
-        </Routes>        
-    );
+export function Rotas() {
+  return (
+    <Routes>
+      <Route path="/catalogo" element={<Inicial />}>
+        <Route index element={<Lista />} /> {/* Rota padrão para /catalogo */}
+        <Route path="perfil" element={<Perfil />} /> {/* /catalogo/perfil */}
+        <Route path="series" element={<ListaSerie />} /> {/* /catalogo/series */}
+      </Route>
+    </Routes>
+  );
 }
